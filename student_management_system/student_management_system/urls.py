99e29e3b6990222d373018e82d61e00c4dd41458
vknan <n.vijaykrishna97@gmail.com>
2022-11-12 19:35:32 +0530

    urls.py, views.py modified

--- student_management_system/student_management_system/urls.py
+++ student_management_system/student_management_system/urls.py
@@ -21,4 +21,5 @@ from .import views,Hod_views, staff_views, student_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('base/', views.BASE, name ='base'),
 ] + static(settings.MEDIA_URL, document_root =  settings.MEDIA_ROOT)
--- student_management_system/student_management_system/views.py
+++ student_management_system/student_management_system/views.py
@@ -1,1 +1,4 @@
 from django.shortcuts import render, redirect
+
+def BASE(request):
+    return render(request, 'base_html')
